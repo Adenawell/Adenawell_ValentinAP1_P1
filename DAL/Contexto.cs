f3e11049ba97cf3336f5048c1cdbc8eaec5db262
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-02-06 21:40:48 -0400

    Creacion de Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Adenawell_ValentinAP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Adenawell_ValentinAP1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; }
+}
