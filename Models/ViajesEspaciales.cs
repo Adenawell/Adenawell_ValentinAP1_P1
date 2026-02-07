d7190ff5ce9d829a5049e77ecf9422c190cbcf17
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-02-06 21:38:32 -0400

    Agregar Modelo ViajesEspaciales

---
+++ Models/ViajesEspaciales.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adenawell_ValentinAP1_P1.Models;
+
+public class ViajesEspaciales
+{
+    [Key]
+    public int VueloId { get; set; }
+
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [Range(1, int.MaxValue, ErrorMessage = "El campo debe ser un numero positivo")]
+    public double Costo { get; set; }
+
+
+
+}
