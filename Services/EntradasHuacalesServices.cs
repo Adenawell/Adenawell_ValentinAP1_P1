36bd2aaaab0bf5c40700c694071dd35221f7da0e
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-02-09 19:01:52 -0400

    Agregando Services

--- Services/EntradasHuacalesServices.cs
+++ Services/EntradasHuacalesServices.cs
@@ -9,34 +9,53 @@ public class EntradasHuacalesServices(IDbContextFactory<Contexto> DbFactory)
 {
 
     
-        public async Task<bool> Guardar()
+        public async Task<bool> Guardar(EntradasHuacales entradasHuacales)
         {
-            return false;
+            if(await Existe(entradasHuacales))
+            {
+              return false;
+            }
+            if(!await Existe(entradasHuacales))
+            {
+              return await Insertar(entradasHuacales);
+            }
+           else
+           {
+             return await Modificar(entradasHuacales);
+           }
+
         }
 
-        private async Task<bool> Existe()
+        private async Task<bool> Existe(EntradasHuacales entradasHuacales)
         {
-            return false;
+          await using var contexto = await DbFactory.CreateDbContextAsync();
+          return await contexto.EntradasHuacales.AnyAsync(e => e.IdEntrada == entradasHuacales.IdEntrada);
         }
 
-        private async Task<bool> Insertar()
+        private async Task<bool> Insertar(EntradasHuacales entradasHuacales)
         {
-            return false;
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.EntradasHuacales.Add(entradasHuacales);
+            return await contexto.SaveChangesAsync() > 0;
         }
 
-        private async Task<bool> Modificar()
+        private async Task<bool> Modificar(EntradasHuacales entradasHuacales)
         {
-            return false;
+         await using var contexto = await DbFactory.CreateDbContextAsync();
+         contexto.EntradasHuacales.Update(entradasHuacales);
+         return await contexto.SaveChangesAsync() > 0;
         }
 
-        public async Task<bool> Buscar()
+        public async Task<EntradasHuacales> Buscar(EntradasHuacales entradasHuacales)
         {
-            return false;
+         await using var contexto = await DbFactory.CreateDbContextAsync();
+         return await contexto.EntradasHuacales.FirstOrDefaultAsync(e => e.NombreCliente == entradasHuacales.NombreCliente);
         }
 
-        public async Task<bool> Eliminar()
+        public async Task<bool> Eliminar(EntradasHuacales entradasHuacales)
         {
-            return false;
+          await using var contexto = await DbFactory.CreateDbContextAsync();
+          return await contexto.EntradasHuacales.Where(e => e.IdEntrada == entradasHuacales.IdEntrada).ExecuteDeleteAsync() > 0;
         }
 
         public async Task<List<EntradasHuacales>> Listar(Expression<Func<EntradasHuacales, bool>> criterio)
