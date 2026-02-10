77be9dd001785e0bf72e4484c059703383caede7
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-02-10 16:52:45 -0400

    Realizando la ultima migracion para publicar

--- Migrations/20260210204614_final.cs
+++ Migrations/20260210204614_final.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Adenawell_ValentinAP1_P1.Migrations
 {
     /// <inheritdoc />
-    public partial class inicial : Migration
+    public partial class final : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
