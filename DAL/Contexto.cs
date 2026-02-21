38005cfc1b490693014dd982ccdf38b4c6136682
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-02-21 15:15:30 -0400

    Modificando contexto para agregar tipos huacales

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,18 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options){}
 
+
     public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TiposHuacales>().HasData(
+            new TiposHuacales { TipoId = 1, Descripcion = "Rojo", Existencia = 0 },
+            new TiposHuacales { TipoId = 2, Descripcion = "Verde", Existencia = 0 },
+            new TiposHuacales { TipoId = 3, Descripcion = "Amarillo", Existencia = 0 }
+        );
+    }
 }
